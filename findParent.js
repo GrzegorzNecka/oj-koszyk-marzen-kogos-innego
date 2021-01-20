0b20c79a5e5c69c716a69dc3a71bb5eea145edb5
GrzegorzNecka <strzypior@gmail.com>
2021-01-20 09:03:21 +0100

    2 files modified

--- findParent.js
+++ findParent.js
@@ -1,7 +1,15 @@
 function findParent(element) {
+  // console.log(element.id);
+
+  //jeśli warunek jest prawdą, polecenia w bloku są wykonywane. Jeśli nie jest, sterowanie przepływa do pierwszego polecenia za pętlą.
+
   while (!element.id) {
+    //jeśli element.id === false
+    // console.log(element);
+    // console.log("parent", element.parentNode);
     element = element.parentNode;
   }
+  // console.log(element);
   return element;
 }
 export default findParent;
--- showSummary.js
+++ showSummary.js
@@ -1,6 +1,12 @@
 import calculateTotalPrice from "./calculateTotalPrice";
 
 function showSummary(summaryContainer, products, container) {
+
+console.log(summaryContainer)
+console.log(products)
+console.log(container)
+
+
   summaryContainer.textContent = `${calculateTotalPrice(
     products,
     container
